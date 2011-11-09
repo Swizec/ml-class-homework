a155b31971b068ad4191bc3fc835f12c14348d07
Swizec Teller <swizec@swizec.com>
2011-11-09 03:06:26 +0100

    calculating cost in silly way

--- mlclass-ex4/nnCostFunction.m
+++ mlclass-ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -46,12 +46,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -62,13 +62,27 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+yy = zeros(size(y),num_labels);
+for i=1:size(X)
+  yy(i,y(i)) = 1;
+end
 
+X = [ones(m,1) X];
 
+i=1;
+for  i=1:m
+  a1 = X(i,:);
+  z2 = Theta1*a1';
+  a2 = sigmoid(z2);
+  z3 = Theta2*[1; a2];
+  a3 = sigmoid(z3);
 
+  for k=1:num_labels
+    J += -yy(i,k)*log(a3(k))-(1-yy(i,k))*log(1-a3(k));
+  end
+end
 
-
-
-
+J /= m
 
 
 
