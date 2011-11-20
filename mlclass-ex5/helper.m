3cf4442a08e7d5beb46f1cfd2acd9812e00581bd
Swizec Teller <swizec@swizec.com>
2011-11-20 23:52:59 +0100

    part 1

---
+++ mlclass-ex5/helper.m
@@ -0,0 +1,10 @@
+load ('ex5data1.mat');
+
+% m = Number of examples
+m = size(X, 1);
+
+theta = [1 ; 1];
+J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+
+fprintf(['Cost at theta = [1 ; 1]: %f '...
+         '\n(this value should be about 303.993192)\n'], J);
