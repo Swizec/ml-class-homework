bf28812956a87cc9aeee457fb21c54461639f4ff
Swizec Teller <swizec@swizec.com>
2011-11-21 00:12:00 +0100

    part 2

--- mlclass-ex5/helper.m
+++ mlclass-ex5/helper.m
@@ -4,7 +4,8 @@ load ('ex5data1.mat');
 m = size(X, 1);
 
 theta = [1 ; 1];
-J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+[J, grad] = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
 
-fprintf(['Cost at theta = [1 ; 1]: %f '...
-         '\n(this value should be about 303.993192)\n'], J);
+fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
+         '\n(this value should be about [-15.303016; 598.250744])\n'], ...
+         grad(1), grad(2));
--- mlclass-ex5/linearRegCostFunction.m
+++ mlclass-ex5/linearRegCostFunction.m
@@ -21,7 +21,8 @@ grad = zeros(size(theta));
 
 J = (1/(2*m))*sum((X*theta-y).^2)+(lambda/(2*m))*(sum(theta(2:end).^2));
 
-
+grad = (1/m)*(X'*(X*theta-y))+(lambda/m)*theta;
+grad(1) -= (lambda/m)*theta(1);
 
 
 % =========================================================================
