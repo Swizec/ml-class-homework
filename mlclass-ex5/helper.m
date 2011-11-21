20e0f5734c4edd5ba36a97a5ac5f00e776a1855e
Swizec Teller <swizec@swizec.com>
2011-11-21 02:13:20 +0100

    part 3

--- mlclass-ex5/helper.m
+++ mlclass-ex5/helper.m
@@ -3,9 +3,21 @@ load ('ex5data1.mat');
 % m = Number of examples
 m = size(X, 1);
 
-theta = [1 ; 1];
-[J, grad] = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+lambda = 0;
+[error_train, error_val] = ...
+    learningCurve([ones(m, 1) X], y, ...
+                  [ones(size(Xval, 1), 1) Xval], yval, ...
+                  lambda);
 
-fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
-         '\n(this value should be about [-15.303016; 598.250744])\n'], ...
-         grad(1), grad(2));
+plot(1:m, error_train, 1:m, error_val);
+title('Learning curve for linear regression')
+legend('Train', 'Cross Validation')
+xlabel('Number of training examples')
+ylabel('Error')
+axis([0 13 0 150])
+
+fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
+for i = 1:m
+    fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
+end
+
