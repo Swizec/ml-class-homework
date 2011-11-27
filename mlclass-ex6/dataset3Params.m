f7e05c7e022002a8349cc091bcd318466d6d010d
Swizec Teller <swizec@swizec.com>
2011-11-28 00:55:37 +0100

    part 2

--- mlclass-ex6/dataset3Params.m
+++ mlclass-ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,19 +15,35 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
 
+vals = [0.01 0.03 0.1 0.3 1 3 10 30]';
+best = 100;
 
+%for i=1:size(vals)
+%  for j=1:size(vals)
+%    model = svmTrain(X, y, vals(i), @(x1, x2) gaussianKernel(x1, x2, vals(j)));
+%    predictions = svmPredict(model, Xval);
+%    m = mean(double(predictions ~= yval));
+%    if m < best
+%      C = vals(i);
+%      sigma = vals(j);
+%      best = m;
+%    end
+%    C, sigma, m
+%  end
+%end
 
+%C, sigma, m
 
-
-
+C = 1;
+sigma = 0.1;
 
 % =========================================================================
 
