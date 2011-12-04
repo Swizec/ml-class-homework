25054370d7814c16d6613ba2be942f981027d0a5
Swizec Teller <swizec@swizec.com>
2011-12-04 02:54:45 +0100

    part 1

---
+++ mlclass-ex7/helper.m
@@ -0,0 +1,13 @@
+load('ex7data2.mat');
+
+% Select an initial set of centroids
+K = 3; % 3 Centroids
+initial_centroids = [3 3; 6 2; 8 5];
+
+% Find the closest centroids for the examples using the
+% initial_centroids
+idx = findClosestCentroids(X, initial_centroids);
+
+fprintf('Closest centroids for the first 3 examples: \n')
+fprintf(' %d', idx(1:3));
+fprintf('\n(the closest centroids should be 1, 3, 2 respectively)\n');
