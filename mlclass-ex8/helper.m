a7b1c79f30bd4aa3ec8c52540a55ea166df8c180
Swizec Teller <swizec@swizec.com>
2011-12-11 19:58:19 +0100

    part 2

--- mlclass-ex8/helper.m
+++ mlclass-ex8/helper.m
@@ -2,3 +2,12 @@
 load('ex8data1.mat');
 
 [mu sigma2] = estimateGaussian(X);
+
+pval = multivariateGaussian(Xval, mu, sigma2);
+
+[epsilon F1] = selectThreshold(yval, pval);
+
+fprintf('Best epsilon found using cross-validation: %e\n', epsilon);
+fprintf('Best F1 on Cross Validation Set:  %f\n', F1);
+fprintf('   (you should see a value epsilon of about 8.99e-05)\n\n');
+
