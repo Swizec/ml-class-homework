a425e1dd19b47d30c8051712fa6314998b2db9b5
Swizec Teller <swizec@swizec.com>
2011-12-11 17:47:35 +0100

    part 1

--- mlclass-ex8/estimateGaussian.m
+++ mlclass-ex8/estimateGaussian.m
@@ -1,11 +1,11 @@
 function [mu sigma2] = estimateGaussian(X)
-%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%ESTIMATEGAUSSIAN This function estimates the parameters of a
 %Gaussian distribution using the data in X
-%   [mu sigma2] = estimateGaussian(X), 
+%   [mu sigma2] = estimateGaussian(X),
 %   The input X is the dataset with each n-dimensional data point in one row
 %   The output is an n-dimensional vector mu, the mean of the data set
 %   and the variances sigma^2, an n x 1 vector
-% 
+%
 
 % Useful variables
 [m, n] = size(X);
@@ -21,10 +21,10 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
-
-
-
-
+mu = (1/m)*sum(X)';
+for j=1:n
+  sigma2(j) = (1/m)*sum((X(:,j)-mu(j)).^2);
+end
 
 
 
---
+++ mlclass-ex8/helper.m
@@ -0,0 +1,4 @@
+
+load('ex8data1.mat');
+
+[mu sigma2] = estimateGaussian(X);
